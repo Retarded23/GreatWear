5675b12f57e6ec4b169fdd4b20afb0b941d18b89
MAYANk-2705 <168637694+Retarded23@users.noreply.github.com>
2025-07-19 13:28:10 +0530

    minor updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+
+  return {
+    plugins: [react(), tailwindcss()],
+   server: {
+    allowedHosts: 'all',
+    host: '0.0.0.0',
+    port: 5173
+  }
+  }
 })
